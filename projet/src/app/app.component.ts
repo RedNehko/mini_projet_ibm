d546ad173ed9747acb9bf417b3a323321517ee3b
Maxime <maxime.lefebvre-isc.france@ibm.com>
2018-04-06 16:27:41 +0200

    Encore changement de titre

--- projet/src/app/app.component.ts
+++ projet/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Voici un titre';
+  title = 'Mini Projet Angular et NodeJS';
 }
