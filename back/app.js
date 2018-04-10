f10a9c986575088700b81c42edea1014ffc63987
Maxime <maxime.lefebvre-isc.france@ibm.com>
2018-04-10 13:02:18 +0200

    maj requete vers bdd

--- back/app.js
+++ back/app.js
@@ -52,6 +52,33 @@ app.use(bodyParser.urlencoded({
 app.options('*', cors());
 app.use(cors({ origin: true, credentials: true }));
 
+
+app.post('/add_serie_film', (req, res) => {
+    console.log(req.body)
+    if (!req.body) {
+        return res.sendStatus(500);
+    } else {
+        // definie la date
+        var date = new Date();
+        var formData = req.body;
+        var title = req.body.title;
+        var type = req.body.type;
+        var description = req.body.description;
+        var addBy = req.body.addBy;
+        var mySerie = new serieFilm({ sftitle: title, sftype: type, sfdescription: description, sfaddBy: addBy, sfaddDate: date });
+        mySerie.save((err, savedSerie) => {
+            if (err) {
+                console.error(err);
+                return;
+            } else {
+                console.log(savedSerie);
+                //res.sendStatus(201);
+                res.status(201).send('La/Le serie/film a été ajouté(e) avec succès !');
+            }
+        });
+    }
+});
+
 require('./router')(app);
 
 var cfenv = require('cfenv');
