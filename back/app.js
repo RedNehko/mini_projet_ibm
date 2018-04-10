a2f23400eb30966487a41d90fc89640732bacccd
Maxime <maxime.lefebvre-isc.france@ibm.com>
2018-04-10 09:20:17 +0200

    Ajout script send data to BDD

--- back/app.js
+++ back/app.js
@@ -1,8 +1,57 @@
+var mongoose = require('mongoose'); // pour la bdd
 var express = require('express');
 var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var router = express.Router();
+
+// on se connecte à la bdd mlab
+mongoose.connect('mongodb://maxime:progtr00@ds237489.mlab.com:37489/mini_projet');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erreur de connexion : Impossible de se connecter à la BDD'));
+db.once('open', () => {
+    console.log('Connecté à la BDD :)');
+});
+
+app.use(express.static('http://localhost:4200/add_serie_film'));//on envoie vers le lien
+
+// structure du schéma
+var sfSchema = mongoose.Schema({
+  sftitle: String,
+  sftype: String,
+  sfdescription: String,
+  sfaddBy: String,
+  sfaddDate: Date
+});
+var serieFilm = mongoose.model('Serie', sfSchema);
+
+// envoie de la nouvelle entrée
+app.post('/http://localhost:4200/add_serie_film', (req, res) => {
+  if(!req.body){
+      return res.sendStatus(500);
+  }else{
+      // definie la date
+      var date = new Date();
+      var formData = req.body;
+      var title = req.body.title;
+      var type = req.body.type;
+      var description = req.body.description;
+      var addBy = req.body.addBy;
+      var mySerie = new Serie({sftitle: title, sftype: type, sfdescription: description, sfaddBy: addBy, sfaddDate: date});
+      mySerie.save((err, savedSerie) => {
+          if(err){
+              console.error(err);
+              return;
+          }else{
+              console.log(savedSerie);
+              //res.sendStatus(201);
+              res.status(201).send('La/Le serie/film a été ajouté(e) avec succès !');
+          }
+      });
+  }
+});
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
