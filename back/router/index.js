33d26ddb9b6a573c3acf0c37850dd291ac88a59c
Maxime <maxime.lefebvre-isc.france@ibm.com>
2018-04-10 16:27:46 +0200

    affichage data all

--- back/router/index.js
+++ back/router/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+var mongoose = require('mongoose'); // pour la bdd
+
 module.exports = function (app) {
 
     app.use('/', router);
@@ -26,4 +28,25 @@ module.exports = function (app) {
         ];
         res.status(200).send(tabMovies);
     })
+
+
+    mongoose.connect('mongodb://maxime:progtr00@ds237489.mlab.com:37489/mini_projet');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Erreur de connexion : Impossible de se connecter à la BDD'));
+    db.once('open', () => {
+        console.log('Connecté à la BDD :)');
+    });
+
+
+    //db.series.find({sftitre: titre});
+    db.collection("series").findOne({}, function(err, result) {
+        if (err) throw err;
+            console.log(result.sftitle);
+        db.close();
+    });
+    db.collection("series").find({type: 'film'}, function(err, result){
+        console.log(result.sftitle);
+    });
+
+
 }
